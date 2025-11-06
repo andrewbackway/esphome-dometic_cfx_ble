1134a44b004e40d8f3ab63cd02e77859b1059ba8
andrewbackway <31836236+andrewbackway@users.noreply.github.com>
2025-11-06 19:39:00 +1100

    a

--- components/dometic_cfx_ble/switch.py
+++ components/dometic_cfx_ble/switch.py
@@ -1,23 +1,19 @@
-# switch.py
 import esphome.codegen as cg
 import esphome.config_validation as cv
-from esphome.const import CONF_DOMETIC_CFX_BLE_ID, CONF_TYPE, CONF_NAME
-from esphome.cpp_types import switch_ as esphome_switch
+from esphome.components import switch as esphome_switch
+from esphome.const import CONF_TYPE
 
-from . import dometic_cfx_ble_ns, DometicCfxBle, TOPIC_TYPES, validate_topic_type
+from . import DometicCfxBle, CONF_DOMETIC_CFX_BLE_ID, validate_topic_type
 
-DometicCfxBleSwitch = dometic_cfx_ble_ns.class_("DometicCfxBleSwitch", esphome_switch.Switch, cg.PollingComponent)
+CONFIG_SCHEMA = esphome_switch.switch_schema().extend(
+    {
+        cv.Required(CONF_DOMETIC_CFX_BLE_ID): cv.use_id(DometicCfxBle),
+        cv.Required(CONF_TYPE): validate_topic_type,
+    }
+)
 
-CONFIG_SCHEMA = esphome_switch.switch_schema(DometicCfxBleSwitch).extend({
-    cv.Required(CONF_DOMETIC_CFX_BLE_ID): cv.use_id(DometicCfxBle),
-    cv.Required(CONF_TYPE): validate_topic_type,
-}).extend(cv.polling_component_schema('60s'))
 
 async def to_code(config):
-    var = cg.new_Pvariable(config[CONF_ID])
-    await cg.register_component(var, config)
-    await esphome_switch.register_switch(var, config)
+    var = await esphome_switch.new_switch(config)
     parent = await cg.get_variable(config[CONF_DOMETIC_CFX_BLE_ID])
     cg.add(parent.add_entity(config[CONF_TYPE], var))
-    cg.add(var.set_parent(parent))
-    cg.add(var.set_topic(config[CONF_TYPE]))
